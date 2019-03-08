600c1e2c4fa59c39446eeeb654de415a5968d63c
wenzhang <wenzhang.wang@baichanghui.com>
2019-03-08 21:00:36 +0800

    try fix HMR bug

---
+++ src/components/Hello/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="hello">
+            <div class="logo"></div>
+            <p @click="testClick">{{hello}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return {
+            hello: '点我'
+        }
+    },
+    methods: {
+        testClick(){
+            this.hello = this.$store.state.text
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .hello {
+        display: flex;
+        flex-flow: column;
+        justify-content: space-around;
+        align-items: center;
+        color: #41b883;
+        font-size: 25px;
+        font-weight: bold;
+        padding-top: 20px;
+        .logo {
+            width: 200px;
+            height: 200px;
+            background-image: url('~@/assets/home/logo.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+    }
+</style>
+
